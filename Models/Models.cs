39b9d7c9a02907a8e339093f29ed13136b80ea4d
Ivan Zulfikar <ivan.zulf.ikar@gmail.com>
2024-03-06 17:07:08 +0700

    create new model

---
+++ Models/Models.cs
@@ -0,0 +1,24 @@
+namespace TheAgencyApi.Models;
+
+// Customer, Appointment, 
+
+public class Customer
+{
+    public int Id { get; set; }
+    public required string FullName { get; set; }
+    public string? Email { get; set; }
+    public string? Phone { get; set; }
+    public string? Address { get; set; }
+    public List<Appointment> Appointments { get; set; } = [];
+}
+
+public class Appointment
+{
+    public int Id { get; set; }
+    public DateTime Date { get; set; }
+    public required Guid Token { get; set; }
+    public required Customer Customer { get; set; }
+    public int CustomerId { get; set; }
+    public string? Location { get; set; }
+    public string? Notes { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,13 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using TheAgencyApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<TheAgencyDbContext>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<TheAgencyDbContext>();
+    dbContext.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
